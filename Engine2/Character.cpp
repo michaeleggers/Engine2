b728fbee10be5bc1f49b6aa6d1b92548ebe41811
Michael Eggers <eggers@hm.edu>
2018-05-22 00:48:17 +0200

    Use diffuse texture for spec-map if no specular texture is available. Note, that this is all a bit messy (see comment in Character.cpp)

--- Engine2/Character.cpp
+++ Engine2/Character.cpp
@@ -15,14 +15,21 @@ void Character::animate(int start, int end, float percent)
 void Character::render()
 {
 	// TODO(Michael): checking if texture is available is not cool!
+	// Maybe use another uniform to tell if specular map is available
+	// or use whole different shader.
 	m_TexturePtr[DIFFUSE]->Bind(0);
 	if (m_TexturePtr[SPECULAR])
 		m_TexturePtr[SPECULAR]->Bind(1);
+	else
+		m_TexturePtr[DIFFUSE]->Bind(1);
 	AnimatedEntity::render();
 	if (equipped) {
 		m_Weapon.getTexturePtr(DIFFUSE)->Bind(0);
 		if (m_Weapon.getTexturePtr(SPECULAR))
 			m_Weapon.getTexturePtr(SPECULAR)->Bind(1);
+		else
+			m_Weapon.getTexturePtr(DIFFUSE)->Bind(1);
 		m_Weapon.render();
 	}
+
 }
